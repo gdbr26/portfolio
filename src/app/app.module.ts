1ace36f9780029caa274da17f3110d90ce78ffb8
gdbr26 <gayathridevi.26@gmail.com>
2020-05-04 04:13:42 +0530

    dodecahedron nailed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SkillComponent } from './home/skill/skill.component';
+import { ContactComponent } from './home/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SkillComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
