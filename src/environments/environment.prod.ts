d0d54fb288aad2ccd5a0ae2ec7b1318ae7ae234f
Uppi28 <upendrabr.93@gmail.com>
2020-11-18 05:08:11 +0530

    Hosting added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCnZ0BEKVSogMWmsiF5yBB5lB-oE2PKRfQ",
+    authDomain: "gdbr-portfolio.firebaseapp.com",
+    databaseURL: "https://gdbr-portfolio.firebaseio.com",
+    projectId: "gdbr-portfolio",
+    storageBucket: "gdbr-portfolio.appspot.com",
+    messagingSenderId: "858748979564",
+    appId: "1:858748979564:web:bea3d5e3fd2696e3a64b49",
+    measurementId: "G-1VPGY6FSXR"
+  }
 };
