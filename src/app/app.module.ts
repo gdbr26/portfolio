9eb18ab7b385344677d4652d3051d508c2aa498c
Uppi28 <upendrabr.93@gmail.com>
2020-11-20 00:49:29 +0530

    about me added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SkillComponent } from './home/skill/skill.component';
 import { ContactComponent } from './home/contact/contact.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SkillComponent,
-    ContactComponent
+    ContactComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
