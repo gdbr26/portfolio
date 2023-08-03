407d3b1ac47ff770ac804148a05e2437163841d0
gdbr26 <gayathridevi.26@gmail.com>
2023-08-03 17:02:06 +0530

    Projects component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,23 @@ import { SkillComponent } from './home/skill/skill.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { ProjectsComponent } from './home/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SkillComponent,
-    AboutmeComponent
+    AboutmeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
