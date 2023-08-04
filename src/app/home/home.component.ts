adc718c8147249fe6eaa9eb16ded6f45b925efad
gdbr26 <gayathridevi.26@gmail.com>
2023-08-04 16:56:46 +0530

    About component restructureed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,63 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+//     let data = [];
+
+// function updateData() {
+// 	data = [];
+// 	let numItems = Math.ceil(Math.random() * 40);
+// 	for(let i=0; i<numItems; i++) {
+// 		data.push(Math.random() * 800);
+// 	}
+// }
+
+// function update() {
+// 	d3.select('svg')
+// 		.selectAll('circle')
+// 		.data(data)
+// 		.join(
+// 			function(enter) {
+// 				return enter
+// 					.append('circle')
+// 					.attr('cy', 50)
+// 					.attr('cx', function(d) {
+// 						return d;
+// 					})
+// 					.attr('r', 40)
+// 					.style('opacity', 0);
+// 			},
+// 			function(update) {
+// 				return update;
+// 			},
+// 			function(exit) {
+// 				return exit
+// 					.transition()
+// 					.duration(1000)
+// 					.attr('cy', 500)
+// 					.remove();
+// 			}
+// 		)
+// 		.transition()
+// 		.duration(1000)
+// 		.attr('cx', function(d) {
+// 			return d;
+// 		})
+// 		.style('opacity', 0.75);
+// }
+
+// function updateAll() {
+// 	updateData();
+// 	update();
+// }
+
+// updateAll();
+
+// for (let i = 0; i < 20; i++) {
+//   setInterval(()=>{
+//       updateAll();
+//   }, 1000);
+// }
+	
   }
 
 }
